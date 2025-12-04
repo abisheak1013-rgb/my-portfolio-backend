c2489db46615d1be6c1634344b41385573c88466
Abisheak R <abisheak1013@gmail.com>
2025-12-04 16:58:14 +0530

    fix: correct Middleware folder path case

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateJWT } = require("../routes/middleware/middleware");
+const { authenticateJWT } = require("../Routes/Middleware/middleware");
 const UserControllers = require("../controller/user.controller");
 const SampleControllers = require("../controller/sample.controller");
 
